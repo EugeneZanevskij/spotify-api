ea12e5739797f05fbdc173d7da2a038ec7d1a91c
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-10 14:01:06 +0300

    feat: create routes for authentification

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,8 +17,61 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors());
 
+const client_id = process.env.CLIENT_ID as string;
+const redirect_uri = process.env.REDIRECT_URI as string;
+const client_secret = process.env.CLIENT_SECRET as string;
+const generateRandomString = (length: number) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
+});
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  const scope = 'user-read-private user-read-email user-top-read';
+  const params = {
+    response_type: 'code',
+    client_id,
+    scope,
+    redirect_uri,
+    state
+  }
+  const URLParams = new URLSearchParams(params);
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    URLParams.toString());
+});
+
+app.get('/callback', function(req, res) {
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+
+  if (state === null) {
+    const params = {
+      error: 'state_mismatch'
+    }
+    const URLParams = new URLSearchParams(params);
+    res.redirect('/#' +
+      URLParams.toString());
+  } else {
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+  }
 });
 
 app.listen(PORT, () => {
