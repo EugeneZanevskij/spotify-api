2e1349101d3e34ce265539714c22b816eef42bc6
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-16 22:52:16 +0300

    feat: create SpotifyIconLink

--- client/src/components/ArtistItem/index.tsx
+++ client/src/components/ArtistItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Artist } from '../../types';
 import { ArtistContainer, Text, ImageContainer, Image, BoldText } from './styles';
+import SpotifyLink from '../SpotifyLink';
 
 const ArtistItem: React.FC<{ artist: Artist, index?: number }> = ( { artist, index } ) => {
   const getFollowers = (followers: number) => {
@@ -16,6 +17,7 @@ const ArtistItem: React.FC<{ artist: Artist, index?: number }> = ( { artist, ind
     <ArtistContainer>
       <BoldText fontSize='1.2em'>{index} {artist.name}</BoldText>
       <Text>{getFollowers(artist.followers.total)}</Text>
+      <SpotifyLink url={artist.external_urls.spotify} />
       {artist.images[0] && 
       <ImageContainer>
         <Image src={artist.images[0].url} alt={artist.name} />
---
+++ client/src/components/SpotifyLink.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { FaSpotify } from 'react-icons/fa';
+
+const SpotifyIconLink = styled.a`
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  text-decoration: none;
+  color: green;
+  margin-top: 0.25rem;
+`;
+
+const SpotifyLink = ({ url }: { url: string }) => (
+  <SpotifyIconLink href={url} target="_blank" rel="noopener noreferrer">
+    {/* <SpotifyIcon src="spotify-icon.png" alt="Spotify" /> */}
+    <FaSpotify size={24}/>
+  </SpotifyIconLink>
+);
+
+export default SpotifyLink;
