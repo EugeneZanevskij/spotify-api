ed100eca3f8d3aba637930511db21455cea35cee
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-17 21:57:13 +0300

    feat: update HomePage

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,20 +1,62 @@
-import React from 'react'
-import { Container, Wrapper, SpotiContainer, BoldText, Text, LoginLink } from './styles';
+import { HomeContainer, Wrapper, SpotiContainer, BoldText, Text, LoginLink, TopLink, PropsWrapper } from './styles';
+import { FaSearch } from "react-icons/fa";
+import { FaRankingStar, FaRepeat } from "react-icons/fa6";
+import { BiSolidPlaylist } from "react-icons/bi";
 
 const Home = ({ token }: { token: string }) => {
+  const props = [
+    {
+      title: "Search",
+      description: "Search for tracks, albums or artists by name switching between 3 categories for search.",
+      icon: <FaSearch size={48} />
+    },
+    {
+      title: "Your own ranking",
+      description: "View your most listened tracks, artists and switch between 3 different time periods (4 weeks, 6 months and All time).",
+      icon: <FaRankingStar size={48} />
+    },
+    {
+      title: "Create playlist",
+      description: "Create a playlist from your personal charts and listen to them directly in your spotify app.",
+      icon: <BiSolidPlaylist size={48} />
+    },
+    {
+      title: "Recently played tracks",
+      description: "Check out your recently played tracks.",
+      icon: <FaRepeat size={48} />
+    }
+  ];
+
   return (
-    <Container>
+    <HomeContainer>
       <SpotiContainer>
-      <BoldText fontSize='1.5em'>Spotify API</BoldText>
-      <Text textAlign='center'>
-        Please login with your spotify account, to see your track or artist ranking!
-      </Text>
-      {!token && <LoginLink href="http://localhost:7000/login">Login</LoginLink>}
+        <BoldText fontSize='1.5em'>Spotify API</BoldText>
+        {!token ? 
+        <>
+          <Text fontSize='1em' textAlign='center'>
+            Please login with your spotify account, to search for tracks, albums or artists. You can see your track or artist ranking!
+          </Text>
+          <LoginLink href="http://localhost:7000/login">Login</LoginLink>
+        </> :
+        <Wrapper>
+          <BoldText fontSize='1.1em'>See stats:</BoldText>
+          <TopLink to="/top/tracks">Top Tracks</TopLink>
+          <TopLink to="/top/artists">Top Artists</TopLink>
+        </Wrapper>
+        }
       </SpotiContainer>
       <Wrapper>
-        1
+        {props.map((prop, index) => (
+          <Wrapper flexDirection='row' key={index}>
+            {prop.icon}
+            <PropsWrapper>
+              <BoldText fontSize='1.1em'>{prop.title}</BoldText>
+              <Text fontSize='1em'>{prop.description}</Text>
+            </PropsWrapper>
+          </Wrapper>
+        ))}
       </Wrapper>
-    </Container>
+    </HomeContainer>
   )
 }
 
--- client/src/pages/Home/styles.tsx
+++ client/src/pages/Home/styles.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { ItemWrapper } from "../../helpers/ItemWrapper";
 import { PageText } from "../../helpers/PageText";
+import { Link } from "react-router-dom";
 
-export const Container = styled.div`
+export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -15,24 +16,24 @@ export const SpotiContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 1rem;
-  gap: 1rem;
+  gap: 0.5rem;
   width: 100%;
   border-radius: 2rem;
   max-width: 25rem;
-  background-color: #b8efb8;
+  background-color: #bee5be;
 `;
 
 export const Wrapper = styled(ItemWrapper)`
   display: flex;
   flex-direction: ${(props)=>props.flexDirection ?  props.flexDirection  : "column"};
   align-items:  ${(props)=>props.alignItems ? props.alignItems  : "center"};
-  gap: 0.5rem;
+  gap: 1rem;
 `;
 
 export const Text = styled(PageText)`
     color:${(props)=>props.color ? props.color :"#131A22" };
     font-size:${(props)=>props.fontSize ? props.fontSize :".9em" };
-    text-align:${(props)=>props.textAlign ? props.textAlign : "left" };
+    text-align:${(props)=>props.textAlign ? props.textAlign : "center" };
 `;
 
 export const BoldText = styled(Text)`
@@ -48,3 +49,29 @@ export const LoginLink = styled.a`
     background-color: #10551e;
     border-radius: 1em;
 `;
+
+export const TopLink = styled(Link)`
+  background-color: #179800;
+  color: #fff;
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #cbe669;
+    color: #333;
+    border: #179800 1px solid;
+  }
+`;
+
+export const PropsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 20rem;
+  width: 100%;
+`
