850434dc596b65dc50e50e71f97e2f14a320528f
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-01-29 20:02:33 +0300

    feat(client): create constants and add homepageProps

---
+++ client/src/constants/index.ts
@@ -0,0 +1,33 @@
+import { FaSearch } from "react-icons/fa";
+import { FaRankingStar, FaRepeat } from "react-icons/fa6";
+import { BiSolidPlaylist } from "react-icons/bi";
+import { IconType } from "react-icons";
+
+interface homepageProp {
+  title: string;
+  description: string;
+  icon: IconType;
+}
+
+export const homepageProps: homepageProp[] = [
+  {
+    title: "Search",
+    description: "Search for tracks, albums or artists by name switching between 3 categories for search.",
+    icon: FaSearch
+  },
+  {
+    title: "Your own ranking",
+    description: "View your most listened tracks, artists and switch between 3 different time periods (4 weeks, 6 months and All time).",
+    icon: FaRankingStar
+  },
+  {
+    title: "Create playlist",
+    description: "Create a playlist from your personal charts and listen to them directly in your spotify app.",
+    icon: BiSolidPlaylist
+  },
+  {
+    title: "Recently played tracks",
+    description: "Check out your recently played tracks.",
+    icon: FaRepeat
+  }
+];
