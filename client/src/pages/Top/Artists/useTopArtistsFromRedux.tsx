4df696dffe52b96afb1b7e9c1dbe34bf0cdd1e95
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-07 17:55:11 +0300

    feat(client): create useTopArtistsFromRedux hook

---
+++ client/src/pages/Top/Artists/useTopArtistsFromRedux.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTopArtistsAsync } from "../../../state/spotify/spotifySlice";
+import { RootState, AppDispatch } from "../../../state/store";
+import { TimeRange } from "../../../types";
+
+const useTopArtistsFromRedux = (timeRange: TimeRange) => {
+  const accessToken = useSelector((state: RootState) => state.auth.accessToken);
+  const dispatch = useDispatch<AppDispatch>();
+  const topArtists = useSelector(
+    (state: RootState) => state.spotify.topArtists,
+  );
+  const loading = useSelector((state: RootState) => state.spotify.loading);
+  const error = !accessToken;
+
+  useEffect(() => {
+    if (accessToken) {
+      dispatch(getTopArtistsAsync({ accessToken, timeRange }));
+    }
+  }, [timeRange, accessToken, dispatch]);
+
+  return { topArtists, loading, error };
+};
+
+export default useTopArtistsFromRedux;
