73176a2fdb5800f28df88dcc91213e1b40d50309
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-08 17:15:42 +0300

    feat(client): update Error component

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 import { TError } from "../types";
+import Login from "./Login";
 
 const ErrorContainer = styled.div`
   border: 2px solid #ff0000;
-  padding: 10px;
+  padding: 1rem;
   background-color: #ffeeee;
-  border-radius: 5px;
+  border-radius: 0.25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.25rem;
 `;
 
 const ErrorStatus = styled.p`
@@ -13,7 +18,7 @@ const ErrorStatus = styled.p`
 `;
 
 const ErrorMessage = styled.p`
-  margin-top: 5px;
+  color: #ff0000;
 `;
 
 const Error: React.FC<{ error: TError }> = ({ error }: { error: TError }) => {
@@ -21,6 +26,7 @@ const Error: React.FC<{ error: TError }> = ({ error }: { error: TError }) => {
     <ErrorContainer>
       <ErrorStatus>{error.status}</ErrorStatus>
       <ErrorMessage>{error.message}</ErrorMessage>
+      {error.status === 401 && <Login />}
     </ErrorContainer>
   );
 };
